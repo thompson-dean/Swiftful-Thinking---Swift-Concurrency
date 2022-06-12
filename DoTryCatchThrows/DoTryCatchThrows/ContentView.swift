8d74ebad8986f316804e3fb8224748fb03a69b2d
Dean Thompson <deanthompson.music@gmail.com>
2022-06-12 14:50:40 +0900

    Tutorial 1 files added. Do, Try, Catch, Throws

---
+++ DoTryCatchThrows/DoTryCatchThrows/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoTryCatchThrows
+//
+//  Created by Dean Thompson on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoTryCatchThrows/DoTryCatchThrows/DoTryCatchThrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchThrowsApp.swift
+//  DoTryCatchThrows
+//
+//  Created by Dean Thompson on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchThrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
